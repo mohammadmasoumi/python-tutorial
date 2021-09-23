1800df0db743f38e1f45cef2b694e4bc7c7dac7c
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-09-23 23:00:06 +0330

    add comment

---
+++ tutorials/02/comment.py
@@ -0,0 +1,12 @@
+# Comments can be used to explain Python code.
+# Comments can be used to make the code more readable.
+# Comments can be used to prevent execution when testing code.
+
+# this is a comment
+print("Hello this is a comment.")
+
+print("Hello this is a comment.") # this is a comment
+
+# comment out code for test
+# print("Hello this is a comment.")
+
