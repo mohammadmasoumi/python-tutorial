f72fd86a2e99d0f8af515d760fa318dd432e8d92
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-09-23 23:27:37 +0330

    update avriables

--- tutorials/02/03_variables.py
+++ tutorials/02/03_variables.py
@@ -8,3 +8,19 @@ aString = "aString"
 
 print(f"anInt: {anInt}")
 print(f"aString: {aString}")
+
+# dynamic type
+x = 10
+x = "x"
+print(f"x: {x}")
+
+# casting - builtin function
+casted_str = str(3)
+casted_int = int(3)
+casted_float = float(3)
+
+print(f"casted_str: {casted_str}")
+print(f"casted_int: {casted_int}")
+print(f"casted_float: {casted_float}")
+
+
