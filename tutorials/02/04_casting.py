b69284ca7b04d094b28f38ba4a2d9fa52fef5553
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-09-24 00:08:24 +0330

    add 04_casting

---
+++ tutorials/02/04_casting.py
@@ -0,0 +1,30 @@
+# Casting
+
+# int cast
+x1 = int(1)
+x2 = int(2.1)
+x3 = int('3')
+
+print(f"x1: {x1}")
+print(f"x2: {x2}")
+print(f"x3: {x3}")
+
+# float cast
+y1 = float(1)
+y2 = float(2.2)
+y3 = float('3')
+y4 = float('4.2')
+
+print(f"y1: {y1}")
+print(f"y2: {y2}")
+print(f"y3: {y3}")
+print(f"y4: {y4}")
+
+# str cast
+z1 = str(1)
+z2 = str(2.2)
+z3 = str('3')
+
+print(f"z1: {z1}")
+print(f"z2: {z2}")
+print(f"z3: {z3}")
