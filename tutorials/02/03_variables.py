61c9bd31573d07b11ac4b3b21839ca044a061b07
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-09-23 23:22:02 +0330

    update variable

---
+++ tutorials/02/03_variables.py
@@ -0,0 +1,10 @@
+
+# Python has no command for declaring a variable.
+
+# A variable is created the moment you first assign a value to it.
+
+anInt = 10
+aString = "aString"
+
+print(f"anInt: {anInt}")
+print(f"aString: {aString}")
