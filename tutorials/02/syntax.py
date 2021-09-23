2453b0d1662d4ceac2d427783d7167e26afc17b2
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-09-23 22:54:33 +0330

    update syntax

--- tutorials/02/syntax.py
+++ tutorials/02/syntax.py
@@ -1,4 +1,39 @@
-if True:
-  print("Hello world")
+# Python syntax
+
+# we usually use 4 spaces for indentation
+
+# if True:
+#     print("Hello world")
+# else:
+#     print("Hello world")
+
+# -------------------------------------------------------------------------------
+# two spaces indentation
+# keep stick to the number pf indentation
+
+# if True:
+#   print("Hello world")
+# else:
+#   print("Hello world")  
+
+
+# -------------------------------------------------------------------------------
+# six spaces indentation
+
+# if True:
+#       print("Hello world")
+# else:
+#       print("Hello world")
+
+# -------------------------------------------------------------------------------
+# Stntax error
+
+a = 5
+if a > 2:
+    print("Hello world")
 else:
-  print("The same indentation")  
+  print("Hello world")
+
+for idx in range(10):
+    print(idx)
+
