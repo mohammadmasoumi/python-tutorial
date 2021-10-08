88c8d0098cfb06308eedee66b5accfa857613a62
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-10-08 23:01:01 +0330

    add answer for hw2

---
+++ advanced/fall-00-01/HW2/python_list.py
@@ -0,0 +1,25 @@
+my_list = []
+
+for _ in range(int(input())):
+    items = input().split()
+    command = items[0]
+
+    if command == 'append':
+        my_list.append(int(items[1]))
+
+    elif command == 'insert':
+        my_list.insert(int(items[1]), int(items[2]))
+
+    elif command == 'print':
+        print(my_list)
+
+    elif command == 'sort':
+        my_list.sort()
+
+    elif command == 'reverse':
+        my_list.reverse()
+
+    elif command == 'pop':
+        my_list.pop()
+    else:
+        print("Wrong command!")
