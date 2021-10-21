71ac8e3ba7b5ba5df7cfecfb10a698a57ab7f087
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-10-21 22:26:18 +0330

    update codes

---
+++ tutorials/03/02_reverse_string.py
@@ -0,0 +1,20 @@
+#
+#
+# s = {'a', 'b', 'c'}
+#
+# for item in s:
+#     print(item)
+#
+# print(s[0])
+
+# a = [1, 2]
+# b = [*a]
+#
+# print(a)
+# print(b)
+
+a = (1, 2)
+b = (*a, )
+
+print(a)
+print(b)
