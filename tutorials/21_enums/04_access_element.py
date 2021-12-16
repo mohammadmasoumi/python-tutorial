813c65c5ab8f7b0c8cc3772cd0940283c7b606d1
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-12-16 08:00:32 +0330

    update access memeber

--- tutorials/21_enums/04_access_element.py
+++ tutorials/21_enums/04_access_element.py
@@ -2,8 +2,24 @@ from enum import Enum
 
 
 class Color(Enum):
+    RED = 1
+    GREEN = 2
+    YELLOW = 3
 
 
+print(Color(1))
+print(Color(3))
+
+print(Color['RED'])
+print(Color['GREEN'])
+print('-----------------------------')
+
+
+class ColorCode(Enum):
     RED = 'red'
-    GREEN = 'green'
     YELLOW = 'yellow'
+    GREEN = 'green'
+
+
+print(ColorCode('red'))
+print(ColorCode['RED'])
