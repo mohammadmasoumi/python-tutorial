1509462dddc65d5540814fedfa87dfbec666a617
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-12-24 12:30:27 +0330

    add c#

---
+++ tutorials/HelloWorld/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+
+        // commands
+        const string ADD_BOOK = "ADDBOOK"; 
+        const string ADD_STUDENT = "ADDSTUDENT"; 
+
+        const string EXIT = "exit";
+
+
+        static void Main(string[] args)
+        {   
+
+            string inputString, command;
+            bool isFinished = false;
+
+            List<string> books = new List<string>();
+            List<string> students = new List<string>();
+            
+            while (!isFinished) 
+            {
+                inputString = Console.ReadLine() ?? "";
+
+                string[] splittedInput =  inputString.Split(" ");
+                command = splittedInput[0];
+
+                Console.WriteLine("inputString: " + inputString);  // DEBUG
+                Console.WriteLine("command: " + command);  // DEBUG
+
+                if (command == EXIT){
+                    break;
+                } else if (command == ADD_BOOK){
+                    Console.WriteLine("I found ADDBOOK command");
+                }
+
+            }
+        }
+    }
+}
