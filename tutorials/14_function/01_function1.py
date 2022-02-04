80b7ec1448a57afb9ed81cd9dbe753c8cb2693cd
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2022-02-04 23:22:22 +0330

    rename files

---
+++ tutorials/14_function/01_function1.py
@@ -0,0 +1,20 @@
+# functions
+
+# 1. params vs args
+# 2. return value
+# 3. function in function
+# 4. closure
+# 5. scope - global and nonlocal keyword
+# 6. *arga and **kwargs
+# 7. `first class object`` (function) pass function as variable
+# 8. lambda - anonymous function
+# 9. positional arguments
+
+
+# simple function
+def adder(a, b):
+
+    return a + b
+
+print(adder(12, 13))
+
