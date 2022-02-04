095217a78843553f93f171111a4c67cb940ffb14
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2022-02-05 01:56:09 +0330

    update hoc

--- beginners/fall2-00-01/20_sample_codes/03_hoc1.py
+++ beginners/fall2-00-01/20_sample_codes/03_hoc1.py
@@ -4,8 +4,10 @@
 
 # The map function applies a function to each member of a collection
 def my_custom_map(func, iterable):
-
     return [func(item) for item in iterable]
+
+def my_custom_filter(func, iterable):
+    return [item for item in iterable if func(item)]
 
 
 def multiply_by_2(item):
@@ -17,3 +19,4 @@ def is_even(x):
 
 
 print(my_custom_map(multiply_by_2, range(5)))
+print(my_custom_filter(is_even, range(5)))
