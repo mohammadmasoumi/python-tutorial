7de0bf782e62fb29d8cedca7f22653b434105792
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2022-02-07 00:12:06 +0330

    add return None

---
+++ beginners/fall2-00-01/20_sample_codes/18_none_return.py
@@ -0,0 +1,18 @@
+# return None
+
+
+def function1():
+    pass
+
+
+def function2():
+    return
+
+
+def function3():
+    return None
+
+
+print(function1())
+print(function2())
+print(function3())
