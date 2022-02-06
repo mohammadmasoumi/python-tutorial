4f952d09d1531202293151824f43771b444b9825
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2022-02-06 23:54:24 +0330

    add global variables

---
+++ beginners/fall2-00-01/20_sample_codes/10_global_scope1.py
@@ -0,0 +1,17 @@
+# scope1 - read global variables
+
+
+a, b, c = 10, 11, 12
+
+
+def function():
+    # reading global variables does not need global key
+    print(f"[INSIDE] a: {a}, id(a): {id(a)}| b: {b}, id(b): {id(b)}| c: {c}, id(c): {id(c)}")
+
+
+print(f"[BEFORE] a: {a}, id(a): {id(a)}| b: {b}, id(b): {id(b)}| c: {c}, id(c): {id(c)}")
+print("-----------------------")
+function()
+print("-----------------------")
+print(f"[AFTER] a: {a}, id(a): {id(a)}| b: {b}, id(b): {id(b)}| c: {c}, id(c): {id(c)}")
+
---
+++ beginners/fall2-00-01/20_sample_codes/12_global_scope3.py
@@ -0,0 +1,15 @@
+a, b, c = 10, 11, 12
+
+
+def function():
+    # change global variables
+    global a, b, c
+    a, b, c = 1, 2, 3
+    print(f"[INSIDE] a: {a}, id(a): {id(a)}| b: {b}, id(b): {id(b)}| c: {c}, id(c): {id(c)}")
+
+
+print(f"[BEFORE] a: {a}, id(a): {id(a)}| b: {b}, id(b): {id(b)}| c: {c}, id(c): {id(c)}")
+print("-----------------------")
+function()
+print("-----------------------")
+print(f"[AFTER] a: {a}, id(a): {id(a)}| b: {b}, id(b): {id(b)}| c: {c}, id(c): {id(c)}")
