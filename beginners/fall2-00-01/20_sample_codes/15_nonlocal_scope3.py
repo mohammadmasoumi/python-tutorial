ff2efcae4f2710c7445f41314641789a25dc82cb
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2022-02-07 00:01:40 +0330

    add nonlocal scope

---
+++ beginners/fall2-00-01/20_sample_codes/15_nonlocal_scope3.py
@@ -0,0 +1,21 @@
+
+def function1():
+    a, b, c = 1, 2, 3
+
+    print(
+        f"[BEFORE-FUNC1] a: {a}, id(a): {id(a)}| b: {b}, id(b): {id(b)}| c: {c}, id(c): {id(c)}")
+
+    def function2():
+        # For changing nonlocal variables from outer scope, you should use nonlocal.
+        nonlocal a, b, c
+        a, b, c = 11, 12, 13
+
+        print(
+            f"[FUNC2] a: {a}, id(a): {id(a)}| b: {b}, id(b): {id(b)}| c: {c}, id(c): {id(c)}")
+
+    function2()
+    print(
+        f"[AFTER-FUNC1] a: {a}, id(a): {id(a)}| b: {b}, id(b): {id(b)}| c: {c}, id(c): {id(c)}")
+
+
+function1()
