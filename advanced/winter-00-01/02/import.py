2fc97bfcbea15b44d9b481f0c4003b7cb2fdcd28
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2022-02-11 23:37:13 +0330

    add second session

---
+++ advanced/winter-00-01/02/import.py
@@ -0,0 +1,10 @@
+# 1. I wanna import a function from another file
+# from mine_sweaper import print_board
+# print_board()
+
+# 2. import whole module
+import mine_sweaper
+mine_sweaper.print_board()
+print(mine_sweaper.board)
+print(mine_sweaper.board_length)
+
