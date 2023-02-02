427d767e366fc401dda8a4a6957bcc6ef8ebed07
mohammad masoumi <mohammad.masoomy74@gmail.com>
2023-02-02 12:07:34 +0330

    update

--- advanced/1402-1403_spring/03/class_prep/06_decorator_usage.py
+++ advanced/1402-1403_spring/03/class_prep/06_decorator_usage.py
@@ -3,3 +3,16 @@
 # logger
 # log into files
 # times
+
+import functools  
+  
+class Count_Calls:  
+    def __init__(self, func):  
+        functools.update_wrapper(self, func)  
+        self.func = func  
+        self.num_calls = 0  
+  
+    def __call__(self, *args, **kwargs):  
+        self.num_calls += 1  
+        print(f"Call{self.num_calls} of {self.func.__name__!r}")  
+        return self.func(*args, **kwargs)  
